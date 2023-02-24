68b179f3d21cc8ebf3421e5c772e3cc6eb6fd687
RUBEN <RUBEN@PREDATOR>
2023-02-24 15:20:55 +0100

    Commit 0

---
+++ src/bicicleta/Bicicleta.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bicicleta;
+
+/**
+ *
+ * @author RUBEN
+ */
+public class Bicicleta {
+
+private int velocidad;
+    private int plato;
+    private int piñon;
+    
+    public Bicicleta() {
+        velocidad = 0;
+        plato = 1;
+        piñon = 1;
+    }
+
+
+
+
+    }
+    
+
