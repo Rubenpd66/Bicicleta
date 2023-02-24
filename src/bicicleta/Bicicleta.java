78a442d442bc8203b62365fc7bf945a951fac6f1
RUBEN <RUBEN@PREDATOR>
2023-02-24 15:28:32 +0100

    commit 0

--- src/bicicleta/Bicicleta.java
+++ src/bicicleta/Bicicleta.java
@@ -10,38 +10,7 @@ package bicicleta;
  */
 public class Bicicleta {
 
-private int velocidad;
-    private int plato;
-    private int piñon;
-    
-    public Bicicleta() {
-        velocidad = 0;
-        plato = 1;
-        piñon = 1;
-    }
-    public int getVelocidad() {
-        return velocidad;
-    }
-    
-    public void setVelocidad(int nuevaVelocidad) {
-        velocidad = nuevaVelocidad;
-    }
-    
-    public int getPlato() {
-        return plato;
-    }
-    
-    public void setPlato(int nuevoPlato) {
-        plato = nuevoPlato;
-    }
-    
-    public int getPiñon() {
-        return piñon;
-    }
-    
-    public void setPiñon(int nuevoPiñon) {
-        piñon = nuevoPiñon;
-    }
+
 
 
 
